26d690a735f39cc937e813988eaee9e8caf99789
losingone <ying30821@gmail.com>
2024-05-12 21:52:49 +0800

    feat: post model

---
+++ model/post.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const postScheme = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, "'user' is required"],
+    },
+    content: {
+      type: String,
+      required: [true, "'content' is required"],
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const Post = mongoose.model('Post', postScheme);
+
+module.exports = Post;
