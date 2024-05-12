e5bac5a0f5df6f352a4433dc79e83331de523804
losingone <ying30821@gmail.com>
2024-05-12 21:54:49 +0800

    feat: posts controller

---
+++ controllers/posts.js
@@ -0,0 +1,24 @@
+const Post = require('../model/post');
+const { handleSuccess } = require('../service/handler');
+
+const posts = {
+  async getPosts(req, res) {
+    const q = req.query.q;
+    const timeSortData =
+      req.query.timeSort === 'asc' ? 'createdAt' : '-createdAt';
+    const posts = await Post.find({ content: { $regex: q, $options: 'i' } })
+      .sort(timeSortData)
+      .populate({
+        path: 'user',
+        select: 'name photo',
+      });
+    handleSuccess(res, posts);
+  },
+  async deletePosts(req, res) {
+    await Post.deleteMany();
+    const posts = await Post.find();
+    handleSuccess(res, posts);
+  },
+};
+
+module.exports = posts;
