7731fb6aafe2113bd873b087aee5d22b6da7d9c6
losingone <ying30821@gmail.com>
2024-05-13 23:37:24 +0800

    feat: delete console.log

--- connections/index.js
+++ connections/index.js
@@ -7,7 +7,4 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose
-  .connect(DB)
-  .then(() => console.log('connect success'))
-  .catch((err) => console.log(err));
+mongoose.connect(DB);
