1056fa5a356ea6789d584d45b11894e7bfb2f08f
losingone <ying30821@gmail.com>
2024-05-14 11:35:34 +0800

    feat: delete index &  user router

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const postsRouter = require('./routes/posts');
 const postRouter = require('./routes/post');
 require('./connections');
@@ -33,8 +31,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api/posts', postsRouter);
 app.use('/api/post', postRouter);
 
