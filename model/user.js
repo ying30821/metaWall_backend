2add61d31f95db3a18651c347d21a4237714c99d
losingone <ying30821@gmail.com>
2024-05-25 23:57:03 +0800

    feat: modify `user` model

--- model/user.js
+++ model/user.js
@@ -9,10 +9,28 @@ const userSchema = new mongoose.Schema(
     email: {
       type: String,
       required: [true, "'email' is required"],
+      unique: true,
+      lowercase: true,
+      select: false,
+    },
+    password: {
+      type: String,
+      required: [true, 'password is required.'],
+      minlength: 8,
+      select: false,
     },
     photo: {
       type: String,
       default: '',
+    },
+    sex: {
+      type: String,
+      enum: ['male', 'female'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      select: false,
     },
   },
   {
