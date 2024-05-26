377b8ad49abb58b30f52947ce73b9624b0982907
losingone <ying30821@gmail.com>
2024-05-26 17:12:01 +0800

    feat: checkAuth

--- service/auth.js
+++ service/auth.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
+const User = require('../model/user');
+const { handleErrorAsync, createAppError } = require('./handler');
 dotenv.config({ path: './config.env' });
 
 const generateToken = (id) => {
@@ -8,5 +10,24 @@ const generateToken = (id) => {
   });
   return token;
 };
+const checkAuth = handleErrorAsync(async (req, res, next) => {
+  const token = req.headers.authorization?.startsWith('Bearer')
+    ? req.headers.authorization.split(' ')[1]
+    : null;
+  if (!token) return next(createAppError(401, 'Unauthorized: Login required'));
+  const decoded = await new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+      if (err) {
+        reject(err);
+        next(createAppError(401, 'Unauthorized: Invalid Token'));
+      } else {
+        resolve(payload);
+      }
+    });
+  });
+  const user = await User.findById(decoded.id);
+  req.user = user;
+  next();
+});
 
-module.exports = { generateToken };
+module.exports = { generateToken, checkAuth };
